0957d3887df34c0673b9c2c2d3aca7d93dc941e8
jrc03c <jrc03c@pm.me>
2022-10-21 14:51:45 -0500

    ...

--- src/helpers/hsl-to-rgb.test.js
+++ src/helpers/hsl-to-rgb.test.js
@@ -8,3 +8,25 @@ test("tests that HSLToRGB works as expected", () => {
   expect(Math.abs(pred.g - truth.g)).toBeLessThan(1)
   expect(Math.abs(pred.b - truth.b)).toBeLessThan(1)
 })
+
+test("tests that invalid values are never produced", () => {
+  for (let h = 0; h < 360; h++) {
+    for (let s = 0; s < 1; s += 0.01) {
+      for (let l = 0; l < 1; l += 0.01) {
+        const { r, g, b } = HSLToRGB(h, s, l)
+
+        if (isNaN(r) || r < 0 || r > 255) {
+          throw new Error()
+        }
+
+        if (isNaN(g) || g < 0 || g > 255) {
+          throw new Error()
+        }
+
+        if (isNaN(b) || b < 0 || b > 255) {
+          throw new Error()
+        }
+      }
+    }
+  }
+})
