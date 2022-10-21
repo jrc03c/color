4329441365438469bc178d4d24b20b9a8522c6ac
jrc03c <jrc03c@pm.me>
2022-10-21 14:59:23 -0500

    ...

--- src/helpers/hsl-to-cmyk.js
+++ src/helpers/hsl-to-cmyk.js
@@ -6,10 +6,15 @@ function HSLToCMYK(h, s, l) {
   const g_ = g / 255
   const b_ = b / 255
   const k = 1 - Math.max(r_, g_, b_)
-  const c = (1 - r_ - k) / (1 - k)
-  const m = (1 - g_ - k) / (1 - k)
-  const y = (1 - b_ - k) / (1 - k)
-  return { c, m, y, k }
+
+  if (k === 1) {
+    return { c: 0, m: 0, y: 0, k: 0 }
+  } else {
+    const c = (1 - r_ - k) / (1 - k)
+    const m = (1 - g_ - k) / (1 - k)
+    const y = (1 - b_ - k) / (1 - k)
+    return { c, m, y, k }
+  }
 }
 
 module.exports = HSLToCMYK
--- src/helpers/hsl-to-cmyk.test.js
+++ src/helpers/hsl-to-cmyk.test.js
@@ -8,3 +8,20 @@ test("tests that HSLToCMYK works as expected", () => {
   expect(Math.abs(pred.y - truth.y)).toBeLessThan(0.01)
   expect(Math.abs(pred.k - truth.k)).toBeLessThan(0.01)
 })
+
+test("tests that invalid values are never produced", () => {
+  for (let h = 0; h < 360; h++) {
+    for (let s = 0; s < 1; s += 0.01) {
+      for (let l = 0; l < 1; l += 0.01) {
+        const { c, m, y, k } = HSLToCMYK(h, s, l)
+        const values = [c, m, y, k]
+
+        values.forEach(value => {
+          if (isNaN(value) || value < 0 || value > 1) {
+            throw new Error()
+          }
+        })
+      }
+    }
+  }
+})
