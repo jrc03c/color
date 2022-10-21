4ed3d444932c8a382b4d730e05aa7bbfc48d2316
jrc03c <jrc03c@pm.me>
2022-10-21 15:10:42 -0500

    ...

--- src/helpers/cmyk-to-hsl.test.js
+++ src/helpers/cmyk-to-hsl.test.js
@@ -7,3 +7,27 @@ test("tests that CMYKToHSL works as expected", () => {
   expect(Math.abs(pred.s - truth.s)).toBeLessThan(0.01)
   expect(Math.abs(pred.l - truth.l)).toBeLessThan(0.01)
 })
+
+test("tests that invalid values are never produced", () => {
+  for (let c = 0; c < 1; c += 0.01) {
+    for (let m = 0; m < 1; m += 0.01) {
+      for (let y = 0; y < 1; y += 0.01) {
+        for (let k = 0; k < 1; k += 0.01) {
+          const { h, s, l } = CMYKToHSL(c, m, y, k)
+
+          if (isNaN(h) || h < 0 || h >= 360) {
+            throw new Error()
+          }
+
+          if (isNaN(s) || s < 0 || s > 1) {
+            throw new Error()
+          }
+
+          if (isNaN(l) || l < 0 || l > 1) {
+            throw new Error()
+          }
+        }
+      }
+    }
+  }
+})
